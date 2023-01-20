da8e0c01e47362ddd883c3f9e05f424df725649f
Fringhian Quentin <quentin.fringhian@epitech.eu>
2023-01-20 17:25:23 +0900

    feat: postcss implementation and basic config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { invalidateAll } from '$app/navigation';
 	import { supabaseClient } from '$lib/supabase';
 	import { onMount } from 'svelte';
+	import '../app.postcss';
 
 	onMount(() => {
 		const {
