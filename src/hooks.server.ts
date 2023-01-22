c6665ca9f7594087f7aab5513586466c7182b384
Fringhian Quentin <quentin.fringhian@epitech.eu>
2023-01-23 01:22:52 +0900

    fix: fetch error

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
 import '$lib/supabase';
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const { session, supabaseClient } = await getSupabase(event);
@@ -8,4 +8,12 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.sb = supabaseClient;
 	event.locals.session = session;
 	return resolve(event);
+};
+
+export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
+	if (request.url.startsWith('http://localhost/')) {
+		const host = event.request.headers.get('host');
+		if (host) request.headers.set('host', host);
+	}
+	return fetch(request);
 };
